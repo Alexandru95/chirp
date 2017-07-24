960983165fdf7be2cac8d57393c5391965b74897
Alexandru <alexandru.tana@yahoo.com>
2017-07-24 10:01:55 +0300

    Added profile view

--- message/views.py
+++ message/views.py
@@ -1,6 +1,8 @@
+# Create your views here.
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
@@ -13,11 +15,28 @@ class RegisterView(CreateView):
     template_name = 'register.html'
     success_url = "/login"
 
-
 class TimelineView(ListView):
     template_name = 'index.html'
     def get_queryset(self):
+        # implement the logic
         if self.request.user.is_authenticated:
             return Message.objects.filter(user=self.request.user)
         else:
             return Message.objects.all()
+
+class ProfileBaseView(DetailView):
+    model = User
+    template_name = 'profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileBaseView, self).get_context_data(**kwargs)
+        context["chirps"] = Message.objects.filter(user=self.get_object())
+        return context
+
+class MyProfileView(ProfileBaseView):
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class ProfileView(ProfileBaseView):
+    def get_slug_field(self):
+        return "username"
