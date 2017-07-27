2c931bab54d9bd674f5c3de86a633dfc23ec189b
Alexandru <alexandru.tana@yahoo.com>
2017-07-27 13:01:27 +0300

    Last changes!!!

--- Static/js/likeDislike.js
+++ Static/js/likeDislike.js
@@ -6,19 +6,42 @@
           var request = $.ajax({
               url: "http://127.0.0.1:8000/like/",
               method: "POST",
-              data: {id : "SUNT ID"}
+              data: {
+                id : $(this).attr("data-messageid"),
+                like: 1
+              }
           });
 
           request.done(function(){
               alert("SUCCESS!");
+              location.reload();
           });
 
           request.fail(function(){
               alert("FAILLLL!");
+              location.reload();
           });
        });
        $('.dislike-message').click(function(){
           alert("Pressed a dislike button!");
+          var request = $.ajax({
+              url: "http://127.0.0.1:8000/like/",
+              method: "POST",
+              data: {
+                id : $(this).attr("data-messageid"),
+                like: 0
+              }
+          });
+
+          request.done(function(){
+              alert("SUCCESS!");
+              location.reload();
+          });
+
+          request.fail(function(){
+              alert("FAILLLL!");
+              location.reload();
+          });
        });
     });
 })();
